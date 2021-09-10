bca5457f22bbfca2c3a4e059456809cafa6c06b7
unknown <surajkumar.4892@gmail.com>
2021-09-10 11:23:25 +0530

    Verify otp Changes

--- src/app/unauthorized/verify-otp/verify-otp.module.ts
+++ src/app/unauthorized/verify-otp/verify-otp.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { IonicModule } from '@ionic/angular';
 
@@ -13,7 +13,8 @@ import { VerifyOtpPage } from './verify-otp.page';
     CommonModule,
     FormsModule,
     IonicModule,
-    VerifyOtpPageRoutingModule
+    VerifyOtpPageRoutingModule,
+    ReactiveFormsModule
   ],
   declarations: [VerifyOtpPage]
 })
