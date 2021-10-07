926d196c2eab99b93ec0f71b02d1ad38c508473f
Jiwannishad <jiwan@arosys.com>
2021-10-07 22:48:54 +0530

    shared copy and table change

--- src/app/common/share-app/share-app.component.ts
+++ src/app/common/share-app/share-app.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ToastController } from '@ionic/angular';
+import { NotificationService } from 'src/app/services/notification/notification.service';
 
 @Component({
   selector: 'app-share-app',
@@ -6,9 +8,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./share-app.component.scss'],
 })
 export class ShareAppComponent implements OnInit {
-
-  constructor() { }
+  public linkvalue='https://play.google.com/store/apps/details?id=com.optimus.currency'
+ 
+  constructor(public toastController: ToastController) { }
 
   ngOnInit() {}
-
+  public async copyContent(value){
+    let selBox = document.createElement('textarea');
+    selBox.value = value;
+    document.body.appendChild(selBox);
+    selBox.select();
+    document.execCommand('copy');
+    document.body.removeChild(selBox);
+    const toast = await this.toastController.create({
+      message: 'Link Copied',
+      duration: 2000,
+      position: 'top',
+      color:'success',
+      buttons: [
+        {
+          side: 'start',
+          icon: 'checkmark-done-outline',}]
+    });
+    toast.present();
+  }
+  
 }
