9f19896fe0850861641ea0c806a1ff1fafe1957e
Jiwannishad <jiwan@arosys.com>
2021-10-13 22:57:36 +0530

    search fix

--- src/app/authorized/networking/networking.page.ts
+++ src/app/authorized/networking/networking.page.ts
@@ -89,8 +89,9 @@ export class NetworkingPage implements OnInit {
     this.treeControl.expandAll();
     text = text.toLowerCase();
     node.forEach((x) => {
-      x.fullname=x.fname+''+x.lname
-      console.log()
+      x.fullname=x.fname +' '+ x.lname+' '
+      console.log(x.fullname)
+    
       x.fullname = x.fullname.toLowerCase();
       x.ok = x.fullname.indexOf(text) >= 0;
       if (x.parent) {
